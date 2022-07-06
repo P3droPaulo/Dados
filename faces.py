c8a8cd64b8dbd3cedd8601ca408deb55caef22a1
P3droPaulo <pedropaulo0624@gmail.com>
2022-07-06 10:18:03 -0300

    Initial commit

---
+++ faces.py
@@ -0,0 +1,50 @@
+import re
+
+
+def one():
+    one = ''' _______
+|       |
+|   o   |
+|       |
+ -------'''
+    return one
+
+def two():
+    two = ''' _______
+| o     |
+|       |
+|     o |
+ -------'''
+    return two
+
+def three():
+    three = ''' _______
+| o     |
+|   o   |
+|     o |
+ -------'''
+    return three
+
+def four(): 
+    four = ''' _______
+| o   o |
+|       |
+| o   o |
+ -------'''
+    return four
+
+def five():
+    five = ''' _______
+| o   o |
+|   o   |
+| o   o |
+ -------'''
+    return five
+
+def six():
+    six = ''' _______
+| o   o |
+| o   o |
+| o   o |
+ -------'''
+    return six
---
+++ main.py
@@ -0,0 +1,10 @@
+import faces
+import os
+
+os.system('cls')
+print(faces.one())
+print(faces.two())
+print(faces.three())
+print(faces.four())
+print(faces.five())
+print(faces.six())
